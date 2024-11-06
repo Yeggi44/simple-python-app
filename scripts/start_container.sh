8c903693371c690c96717448a9d8ad12c18eb1ed
Yagneshkumar Mangroliya <105581569+Yeggi44@users.noreply.github.com>
2024-11-06 04:24:23 -0600

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 set -e
 
-<<<<<<< HEAD
+
 # Pull the Docker image from Docker Hub
-echo
-
-# Run the Docker image as a container
-echo
-=======
-
->>>>>>> 80640e7ae5b4897682a8f486aca5423a23d12063
+echo "hello"
