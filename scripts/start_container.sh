ea88e59d939f8a1c7940a8476e9fcdcf45666a22
Yagneshkumar Mangroliya <105581569+Yeggi44@users.noreply.github.com>
2024-11-10 07:22:08 -0600

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
-
 # Pull the Docker image from Docker Hub
-echo "hello"
+docker pull yagnesh5/simple-python-flask-app
+
+
+# Run the Docker image as a container
+docker run -d -p 5000:5000 yagnesh5/simple-python-flask-app
